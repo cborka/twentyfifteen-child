439a2365273a6e47c3fe232dd0a6c76eddc47756
Boris Ts. (cborka) <cborka777@yandex.ru>
2022-05-06 16:04:42 +0700

    2022-05-06 mix

--- twentyfifteen-child/footer.php
+++ twentyfifteen-child/footer.php
@@ -36,54 +36,75 @@
             <br>
             ---
             <br>
+
             <!-- Yandex.Metrika informer -->
             <a href="https://metrika.yandex.ru/stat/?id=53690932&amp;from=informer"
-            target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/53690932/3_0_FFFFFFFF_EFEFEFFF_0_uniques"
-            style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" class="ym-advanced-informer" data-cid="53690932" data-lang="ru" /></a>
+               target="_blank" rel="nofollow"><img src="https://informer.yandex.ru/informer/53690932/3_1_FFFFFFFF_EFEFEFFF_0_uniques"
+                                                   style="width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" class="ym-advanced-informer" data-cid="53690932" data-lang="ru" /></a>
             <!-- /Yandex.Metrika informer -->
 
             <!-- Yandex.Metrika counter -->
             <script type="text/javascript" >
-                (function (d, w, c) {
-                    (w[c] = w[c] || []).push(function() {
-                        try {
-                            w.yaCounter53690932 = new Ya.Metrika({
-                                id:53690932,
-                                clickmap:true,
-                                trackLinks:true,
-                                accurateTrackBounce:true,
-                                trackHash:true
-                            });
-                        } catch(e) { }
-                    });
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-                    var n = d.getElementsByTagName("script")[0],
-                        s = d.createElement("script"),
-                        f = function () { n.parentNode.insertBefore(s, n); };
-                    s.type = "text/javascript";
-                    s.async = true;
-                    s.src = "https://mc.yandex.ru/metrika/watch.js";
-
-                    if (w.opera == "[object Opera]") {
-                        d.addEventListener("DOMContentLoaded", f, false);
-                    } else { f(); }
-                })(document, window, "yandex_metrika_callbacks");
+                ym(53690932, "init", {
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    trackHash:true
+                });
             </script>
             <noscript><div><img src="https://mc.yandex.ru/watch/53690932" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
             <!-- /Yandex.Metrika counter -->
 
 
+
+            <!-- Rating Mail.ru counter -->
+            <script type="text/javascript">
+                var _tmr = window._tmr || (window._tmr = []);
+                _tmr.push({id: "2945281", type: "pageView", start: (new Date()).getTime()});
+                (function (d, w, id) {
+                    if (d.getElementById(id)) return;
+                    var ts = d.createElement("script"); ts.type = "text/javascript"; ts.async = true; ts.id = id;
+                    ts.src = "https://top-fwz1.mail.ru/js/code.js";
+                    var f = function () {var s = d.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ts, s);};
+                    if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); }
+                })(document, window, "topmailru-code");
+            </script><noscript><div>
+                    <img src="https://top-fwz1.mail.ru/counter?id=2945281;js=na" style="border:0;position:absolute;left:-9999px;" alt="Top.Mail.Ru" />
+                </div></noscript>
+            <!-- //Rating Mail.ru counter -->
+            <!-- Rating Mail.ru logo -->
+            <a href="https://top.mail.ru/jump?from=2945281">
+                <img src="https://top-fwz1.mail.ru/counter?id=2945281;t=479;l=1" style="border:0;" height="31" width="88" alt="Top.Mail.Ru" /></a>
+            <!-- //Rating Mail.ru logo -->
+
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-96451430-1"></script>
+            <script>
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'UA-96451430-1');
+            </script>
+
+            <!--LiveInternet counter--><a href="https://www.liveinternet.ru/click"
+                                          target="_blank"><img id="licntD8D4" width="88" height="31" style="border:0"
+                                                               title="LiveInternet: показано число просмотров за 24 часа, посетителей за 24 часа и за сегодня"
+                                                               src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAEALAAAAAABAAEAAAIBTAA7"
+                                                               alt=""/></a><script>(function(d,s){d.getElementById("licntD8D4").src=
+                    "https://counter.yadro.ru/hit?t14.12;r"+escape(d.referrer)+
+                    ((typeof(s)=="undefined")?"":";s"+s.width+"*"+s.height+"*"+
+                        (s.colorDepth?s.colorDepth:s.pixelDepth))+";u"+escape(d.URL)+
+                    ";h"+escape(d.title.substring(0,150))+";"+Math.random()})
+                (document,screen)</script><!--/LiveInternet-->
+
+
 		</div><!-- .site-info -->
 
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-96451430-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-96451430-1');
-</script>
 
 
 	</footer><!-- .site-footer -->
